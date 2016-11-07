6ab1198203421cc95c3248b86e7e5d050577b5ee
Ian Blakley <ianhblakley@gmail.com>
2016-11-07 16:58:06 -0500

    GA-4 Train Neural Networks: switched to threading and smaller data files

--- python/datagen/create_records.py
+++ python/datagen/create_records.py
@@ -1,23 +1,58 @@
 import os
 import sys
+import threading
+import time
+from multiprocessing import Value
 
 import tensorflow as tf
 
 import game
 
 GAME_DIR = '../../../kgs/sgf'
-OUTPUT_FILE = 'fuseki.tfrecords'
+OUTPUT_FILE = 'data/fuseki-{0}-{1}.tfrecords'
+GAMES_PER_FILE = 1000
+THREADS = 4
 
 tf.app.flags.DEFINE_integer("validation_size", 10000,
                             "Number of boards to separate from training data for validation set")
 FLAGS = tf.app.flags.FLAGS
 
-print "Writing to ", OUTPUT_FILE
-writer = tf.python_io.TFRecordWriter(OUTPUT_FILE)
-count = 0
-for game_file in os.listdir(GAME_DIR):
-    sys.stdout.write('\rWrote {0} games. Current Game from {1}                    '.format(count, game_file))
-    sys.stdout.flush()
-    game.Game(os.path.join(GAME_DIR, game_file), writer)
-    count += 1
-writer.close()
+counter_lock = threading.Lock()
+
+
+def write_file(thread_id, files, thread_game_counter):
+    print "Started thread", thread_id
+    file_count = 0
+    game_count = 0
+    writer = tf.python_io.TFRecordWriter(OUTPUT_FILE.format(thread_id, file_count))
+    for game_file in files:
+        if game_count == GAMES_PER_FILE:
+            writer.close()
+            file_count += 1
+            game_count = 0
+            writer = tf.python_io.TFRecordWriter(OUTPUT_FILE.format(thread_id, file_count))
+            print "Creating new file {0}".format(OUTPUT_FILE.format(thread_id, file_count))
+        game.Game(os.path.join(GAME_DIR, game_file), writer)
+        with counter_lock:
+            sys.stdout.write(
+                '\rWrote {0} games. Current Game from {1}                    '.format(thread_game_counter.value,
+                                                                                      game_file))
+            thread_game_counter.value += 1
+        sys.stdout.flush()
+        game_count += 1
+    writer.close()
+
+
+threads = []
+game_files = os.listdir(GAME_DIR)
+game_counter = Value('i', 0)
+for i in range(THREADS):
+    t = threading.Thread(
+        target=write_file, args=[i, game_files[i * len(game_files) / THREADS: (i + 1) * len(game_files) / THREADS],
+                                 game_counter])
+    threads.append(t)
+    t.daemon = True
+    t.start()
+
+while threading.active_count() > 0:
+    time.sleep(0.1)
